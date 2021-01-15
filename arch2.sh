7d1702699f634c478d5125873b58d1828632599f
gransplash <gransplash@gmail.com>
2021-01-15 22:05:55 +0200

    Add files via upload

--- arch2.sh
+++ arch2.sh
@@ -75,7 +75,40 @@ pacman -S networkmanager network-manager-applet ppp --noconfirm
 echo 'Подключаем автозагрузку менеджера входа и интернет'
 systemctl enable NetworkManager
 
-echo 'Установка завершена! Перезагрузите систему.'
-echo 'Если хотите подключить AUR, установить мои конфиги XFCE, тогда после перезагрзки и входа в систему, установите wget (sudo pacman -S wget) и выполните команду:'
-echo 'wget git.io/arch3.sh && sh arch3.sh'
-exit
+mkdir ~/downloads
+cd ~/downloads
+
+echo 'Установка AUR (yay)'
+sudo pacman -Syu
+sudo pacman -S wget --noconfirm
+wget git.io/yay-install.sh && sh yay-install.sh --noconfirm
+
+echo 'Создаем нужные директории'
+sudo pacman -S xdg-user-dirs --noconfirm
+xdg-user-dirs-update
+
+echo 'Установка базовых программ и пакетов'
+sudo pacman -S firefox firefox-i18n-ru ufw f2fs-tools dosfstools ntfs-3g alsa-lib alsa-utils file-roller p7zip unrar gvfs aspell-ru pulseaudio pavucontrol --noconfirm
+
+echo 'Установить рекомендумые программы?'
+read -p "1 - Да, 0 - Нет: " prog_set
+if [[ $prog_set == 1 ]]; then
+  #Можно заменить на pacman -Qqm > ~/.pacmanlist.txt
+  sudo pacman -S chromium flameshot obs-studio vlc filezilla gimp libreoffice libreoffice-fresh-ru kdenlive neofetch qbittorrent galculator telegram-desktop --noconfirm
+  yay -Syy
+  yay -S xflux sublime-text-dev ttf-symbola ttf-clear-sans --noconfirm
+elif [[ $prog_set == 0 ]]; then
+  echo 'Установка программ пропущена.'
+fi
+
+echo 'Включаем сетевой экран'
+sudo ufw enable
+
+echo 'Добавляем в автозагрузку:'
+sudo systemctl enable ufw
+
+# Очистка
+rm -rf ~/downloads/
+
+echo 'Установка завершена!'
+
